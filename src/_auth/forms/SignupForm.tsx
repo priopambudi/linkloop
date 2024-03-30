3f06ee35b9cb799f9d60e942fdd6d7ed890f0f5e
priopambudi <priopambudi869@gmail.com>
2024-03-30 13:22:45 +0700

    test

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -113,7 +113,7 @@ const SignupForm = () => {
           </Button>
 
           <p className="text-small-regular text-light-2 text-center mt-2">
-            Already have an account?{' '}
+            Already have an account?
             <Link
               to={'/sign-in'}
               className="text-primary-500 text-small-semibold ml-1"
